a6de536ab1c76fefdd66e7e1c6bab2f5dd2d6527
Mustafa Abdulle <mustafa.abdulle@student.nbi-handelsakademin.se>
2023-03-08 19:26:46 +0100

    Database class and Mudel

---
+++ RPSmartHome/dbHelper.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSmartHome
+{
+    internal class dbHelper
+    {
+        // Database connection variable
+
+        public static string server = "localhost";
+        public static string database = "muabhotel";
+        public static string user = "root";
+        public static string pass = "0909";            //Your Database password
+
+
+        MySqlConnection conn = new MySqlConnection($"SERVER={server};DATABASE={database};UID={user};PASSWORD={pass};");
+
+
+    }
+}
